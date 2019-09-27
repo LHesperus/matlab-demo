83db7c3930db1fe75802b5c1000304619a9f91fb
LHesperus <2639350497@qq.com>
2019-09-27 23:21:36 +0800

    --

---
+++ figure/plot3dcircular/readme.md
@@ -0,0 +1,9 @@
+## 画3维圆弧
+
+### plot3dcircle
+*[a] 起点坐标
+*[b] 终点坐标
+*[center] 圆心坐标
+*[dir] 入射方向
+*[theta] 圆弧的角度
+*theta=plot3dcircle(a,b,center,dir)
---
+++ figure/plot3dcircular/test.m
@@ -0,0 +1,11 @@
+% ≤‚ ‘≥Ã–Ú
+close all
+figure
+a=[0,1,0];b=[1,0,0];center=[0,0,0];plot3dcircle(a,b,center,[1,1,0])
+%a=[0,1,0];b=[1,0,0];center=[0,0,0];plot3dcircle(a,b,center,[1,-1,0])
+%a=[0,1,0];b=[1,0,0];center=[0,0,0];plot3dcircle(a,b,center,[-1,1,0])
+%a=[0,1,0];b=[1,0,0];center=[0,0,0];plot3dcircle(a,b,center,[-1,-1,0])
+%a=[0,1,0];b=[1,0,0];center=[0,0,0];plot3dcircle(b,a,center,[1,1,0])
+%a=[0,1,0];b=[1,0,0];center=[0,0,0];plot3dcircle(b,a,center,[1,-1,0])
+%a=[0,1,0];b=[1,0,0];center=[0,0,0];plot3dcircle(b,a,center,[-1,1,0])
+%a=[0,1,0];b=[1,0,0];center=[0,0,0];plot3dcircle(b,a,center,[-1,-1,0])
